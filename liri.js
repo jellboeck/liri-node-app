cdeda3365fe723b9c515a6485d8496f0733d020d
Julia Ellboeck <juliaellboeck@gmail.com>
2019-10-13 12:52:30 -0400

    set up main functions

--- liri.js
+++ liri.js
@@ -1,3 +1,82 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action, value) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotify();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+};
+
+
+function concertThis(value) {
+    var aritist = value.slice().join(" ");
+    console.log(aritist);
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(
+        function (response) {
+            console.log("Venue: " + response.data[0].venue.name);
+            console.log("City: " + response.data[0].venue.city);
+            console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
+        })
+}
+
+function spotify() {
+
+
+};
+
+function movieThis() {
+    var movieName = value.slice().join(" ");
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=[trilogy]";
+
+    axios.get(queryURL).then(
+        function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("Rated: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+
+        }
+    );
+};
+
+
+
+function doWhatItSays() {
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
